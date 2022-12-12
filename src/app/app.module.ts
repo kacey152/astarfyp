0a81f5f0a86a9421e0c5f4b75443861fa972e2af
kacey152 <110165967+kacey152@users.noreply.github.com>
2022-12-12 20:33:39 +0800

    added angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import { ProductionInventoryBComponent } from './components/production-inventory
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatProgressBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/production-inventory/production-inventory.component.ts
+++ src/app/components/production-inventory/production-inventory.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-production-inventory',
   templateUrl: './production-inventory.component.html',
   styleUrls: ['./production-inventory.component.css']
 })
-export class ProductionInventoryComponent implements OnInit {
 
+
+
+export class ProductionInventoryComponent implements OnInit {
+  progressValue: number = 50;
   constructor() { }
 
   ngOnInit(): void {
