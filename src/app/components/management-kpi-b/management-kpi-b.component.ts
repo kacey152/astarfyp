44e48f974acd57c08658d4565e1a9bbd5065dd25
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-01-10 19:37:52 +0800

    completed management kpi B component

--- src/app/components/management-kpi-b/management-kpi-b.component.ts
+++ src/app/components/management-kpi-b/management-kpi-b.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./management-kpi-b.component.css']
 })
 export class ManagementKpiBComponent implements OnInit {
-
+  productivity={
+    value: 1.87,
+    projection: 1.92,
+    target: 2
+  }  
+  onTimeDelivery={
+    value: 99.26,
+    projection: 21.14,
+    target: 99.22 
+  }    
+  capacityUtilization={
+    value: 63.71,
+    projection: 42.96,
+    target: 99.9
+  }    
+  safety={
+    value: 0,
+    projection: 0,
+    target: 0
+  }    
   constructor() { }
 
   ngOnInit(): void {
