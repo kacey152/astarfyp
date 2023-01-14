a2da5de58806bbb30d539a5d047195e56cd6205c
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-01-14 13:26:28 +0800

    created left column of customer satisfaction

--- src/app/components/customer-satisfaction/customer-satisfaction.component.ts
+++ src/app/components/customer-satisfaction/customer-satisfaction.component.ts
@@ -25,11 +25,58 @@ export class CustomerSatisfactionComponent implements OnInit {
         borderColor: 'cyan',
         borderWidth: '1',
         fill: true,
-        
       },
     ],
-    
   };
+
+  directSalesData = {
+    // values on X-Axis
+    labels: [
+      'Date 1',
+      'Date 2',
+      'Date 3',
+      'Date 4',
+      'Date 5',
+      'Date 6',
+      'Date 7',
+    ],
+    datasets: [
+      {
+        label: 'Direct Sales', //chartId
+        data: ['2', '2', '5', '5', '5', '5', '5'],
+        backgroundColor: 'rgba(0, 255, 255, 0.2)',
+        borderColor: 'cyan',
+        borderWidth: '1',
+        fill: true,
+      },
+    ],
+  };
+
+  indirectSalesData = {
+    // values on X-Axis
+    labels: [
+      'Date 1',
+      'Date 2',
+      'Date 3',
+      'Date 4',
+      'Date 5',
+      'Date 6',
+      'Date 7',
+    ],
+    datasets: [
+      {
+        label: 'Indirect Sales', //chartId
+        data: ['3', '5', '5', '5', '5', '4', '4'],
+        backgroundColor: 'rgba(0, 255, 255, 0.2)',
+        borderColor: 'cyan',
+        borderWidth: '1',
+        fill: true,
+      },
+    ],
+  };
+
+
+
   constructor() {}
 
   ngOnInit(): void {}
