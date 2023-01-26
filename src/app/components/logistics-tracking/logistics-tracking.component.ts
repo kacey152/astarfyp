0553ec97c7ff99a3ed63ac807b29669ee9eb3096
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-01-26 20:11:27 +0800

    completed logistics tracking component except for map

--- src/app/components/logistics-tracking/logistics-tracking.component.ts
+++ src/app/components/logistics-tracking/logistics-tracking.component.ts
@@ -21,6 +21,93 @@ export class LogisticsTrackingComponent implements OnInit {
     },
   ];
 
+  orderStatusData = {
+    // values on X-Axis
+    labels: [
+      'Payment Received',
+      'Order Placed',
+      'Production Scheduled',
+      'Production Started',
+      'Production Completed',
+      'In Transit',
+      'Order Delivered',
+    ],
+    datasets: [
+      {
+        label: 'Order Completed', //chartId
+        data: [11, 50, 25, 30, 10, 20, 0],
+        backgroundColor: 'green',
+        borderColor: 'green',
+        borderWidth: '1',
+        fill: true,
+      },
+      {
+        label: 'Order Incomplete', //chartId
+        data: [2, 10, 5, 6, 2, 4, 0],
+        backgroundColor: 'red',
+        borderColor: 'red',
+        borderWidth: '1',
+        fill: true,
+      },
+    ],
+  };
+
+  orders = [{
+    orderNo: "EX-220822-0001",
+    customer: "Test",
+    accountType: "Direct Sales",
+    status: true,
+    planned: "2022-08-22 12:04",
+    actual: "-" 
+  },
+  {
+    orderNo: "EX-220822-0002",
+    customer: "Tan",
+    accountType: "Direct Sales",
+    status: false,
+    planned: "2022-08-21 00:00",
+    actual: "2022-08-22 00:00" 
+  },
+  {
+    orderNo: "EX-220822-0002",
+    customer: "Tan",
+    accountType: "Direct Sales",
+    status: false,
+    planned: "2022-08-21 00:00",
+    actual: "2022-08-22 00:00" 
+  },
+  {
+    orderNo: "EX-220822-0002",
+    customer: "Tan",
+    accountType: "Direct Sales",
+    status: false,
+    planned: "2022-08-21 00:00",
+    actual: "2022-08-22 00:00" 
+  },
+  {
+    orderNo: "EX-220822-0002",
+    customer: "Tan",
+    accountType: "Direct Sales",
+    status: false,
+    planned: "2022-08-21 00:00",
+    actual: "2022-08-22 00:00" 
+  },
+  {
+    orderNo: "EX-220822-0002",
+    customer: "Tan",
+    accountType: "Direct Sales",
+    status: false,
+    planned: "2022-08-21 00:00",
+    actual: "2022-08-22 00:00" 
+  },
+  {
+    orderNo: "EX-220822-0002",
+    customer: "Tan",
+    accountType: "Direct Sales",
+    status: false,
+    planned: "2022-08-21 00:00",
+    actual: "2022-08-22 00:00" 
+  },]
   constructor() {}
 
   ngOnInit(): void {}
