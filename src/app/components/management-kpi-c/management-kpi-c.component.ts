0ef279ec96fea09bdd19e6daf148a35bac9297bb
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-01-28 12:58:55 +0800

    started with management kpi c graph but with error

--- src/app/components/management-kpi-c/management-kpi-c.component.ts
+++ src/app/components/management-kpi-c/management-kpi-c.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit,Output,OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-management-kpi-c',
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./management-kpi-c.component.css']
 })
 export class ManagementKpiCComponent implements OnInit {
+  @Output() categorySelected = new EventEmitter<any>()
+  financials = {
+    // values on X-Axis
+    labels: [
+      'Material',
+      'Others',
+      'Maintenance',
+      'Profit',
+      'Labour',
+    ],
+    datasets: [
+      {
+        label: 'Current Revenue', //chartId
+        data: [21954.6, 1829.5, 29272.8, 14636, 3659.1],
+        backgroundColor: 'green',
+        borderColor: 'green',
+        borderWidth: '1',
+        fill: true,
+      },
+    ],
+  };
 
-  constructor() { }
+
+  changeCategory(category){
+    this.categorySelected.emit(category)
+  }
+
+  constructor() {
+
+   }
 
   ngOnInit(): void {
+    this.changeCategory(this.financials)
   }
+
 
 }
