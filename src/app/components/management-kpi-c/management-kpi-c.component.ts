22f0f512c1aad4f2484f498d4c8a79ac4b38d405
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-01-30 16:34:18 +0800

    split into 2 charts for management kpi c

--- src/app/components/management-kpi-c/management-kpi-c.component.ts
+++ src/app/components/management-kpi-c/management-kpi-c.component.ts
@@ -1,48 +1,56 @@
-import { Component, EventEmitter, OnInit,Output,OnChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  OnChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-management-kpi-c',
   templateUrl: './management-kpi-c.component.html',
-  styleUrls: ['./management-kpi-c.component.css']
+  styleUrls: ['./management-kpi-c.component.css'],
 })
 export class ManagementKpiCComponent implements OnInit {
-  @Output() categorySelected = new EventEmitter<any>()
-  categorySelectedData: any
-  
+  @Output() categorySelected = new EventEmitter<any>();
+  categorySelectedCurrentData: any;
+  categorySelectedProjectedData: any;
+
   financials = {
-    // values on X-Axis
-    labels: [
-      'Material',
-      'Others',
-      'Maintenance',
-      'Profit',
-      'Labour',
-    ],
-    datasets: [
-      {
-        label: 'Current Revenue', //chartId
-        data: [21954.6, 1829.5, 29272.8, 14636, 3659.1],
-        borderWidth: '0',
-      },
-    ],
+    current: {
+      // values on X-Axis
+      labels: ['Material', 'Others', 'Maintenance', 'Profit', 'Labour'],
+      datasets: [
+        {
+          label: 'Current Revenue', //chartId
+          data: [21954.6, 1829.5, 29272.8, 14636, 3659.1],
+          borderWidth: '0',
+        },
+      ],
+    },
+    projected: {
+      labels: ['Material', 'Others', 'Maintenance', 'Profit', 'Labour'],
+      datasets: [
+        {
+          label: 'Projected Revenue', //chartId
+          data: [21954.6, 1829.5, 29272.8, 14636, 3659.1],
+          borderWidth: '0',
+        },
+      ],
+    }
   };
 
-
-  changeCategory(category){
-    this.categorySelected.emit(category)
+  changeCategory(category) {
+    this.categorySelected.emit(category);
   }
 
-  constructor() {
-
-   }
+  constructor() {}
 
   ngOnInit(): void {
     this.categorySelected.subscribe((data) => {
-      this.categorySelectedData = data;
+      this.categorySelectedCurrentData = data.current;
+      this.categorySelectedProjectedData = data.projected;
     });
-    this.changeCategory(this.financials)
+    this.changeCategory(this.financials);
   }
-
-
-
 }
