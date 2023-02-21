22ce956e532819ae1667145e59f31bc164303a42
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-02-21 12:17:42 +0800

    first draft of production-tracking-b mrt system

--- src/app/components/production-tracking-b/production-tracking-b.component.ts
+++ src/app/components/production-tracking-b/production-tracking-b.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductionTrackingBComponent implements OnInit {
   orderNumber: String = "SAMPLE"
+
+  circles = [
+    { x: 100, y: 300, value: 1, status: 'good' },
+    { x: 100, y: 150, value: 2, status: 'warning' },
+    { x: 300, y: 150, value: 3, status: 'danger' },
+    // add more circles as needed
+  ];
+  
+
+  getColor(status: string): string {
+    switch (status) {
+      case 'good': return 'green';
+      case 'warning': return 'yellow';
+      case 'danger': return 'red';
+      default: return 'gray';
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
