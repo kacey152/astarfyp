b78aea842065e23a737b74087def65a80b8411b7
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-03-07 15:16:54 +0800

    added power, electrical and disposal icons with status linking in resource tracking

--- src/app/components/resource-tracking/resource-tracking.component.ts
+++ src/app/components/resource-tracking/resource-tracking.component.ts
@@ -9,6 +9,42 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input } from '
 })
 
 export class ResourceTrackingComponent {
+  mts =[
+    {name:"Injection Moulding", powerStatus:"", electricalStatus:"", disposalStatus:"warning"},
+    {name:"InfraRed Oven 1", powerStatus:"bad", electricalStatus:"bad", disposalStatus:""},
+    {name:"Infrared Oven 2", powerStatus:"bad", electricalStatus:"bad", disposalStatus:"bad"},
+    {name:"Screen Printer 1", powerStatus:"bad", electricalStatus:"bad", disposalStatus:""},
+    {name:"Screen Printer 2", powerStatus:"bad", electricalStatus:"bad", disposalStatus:""},
+    {name:"Membrane Assembly", powerStatus:"", electricalStatus:"", disposalStatus:""},
+    {name:"Laser Welding", powerStatus:"bad", electricalStatus:"bad", disposalStatus:""},
+    {name:"Laser Trimming", powerStatus:"bad", electricalStatus:"bad", disposalStatus:""},
+    {name:"Cartridge Tester", powerStatus:"bad", electricalStatus:"bad", disposalStatus:""},
+]
+  ses =[
+    {name:"Milling", powerStatus:"", electricalStatus:"", disposalStatus:""},
+    {name:"EDM", powerStatus:"", electricalStatus:"bad", disposalStatus:""},
+    {name:"CMM", powerStatus:"", electricalStatus:"bad", disposalStatus:""},
+    {name:"Robotic Arm", powerStatus:"", electricalStatus:"", disposalStatus:""},
+]
+  mto =[
+    {name:"Holder Assembly", powerStatus:"good", electricalStatus:"bad", disposalStatus:""},
+    {name:"Scent Filling", powerStatus:"bad", electricalStatus:"bad", disposalStatus:""},
+    {name:"Holder Tester", powerStatus:"", electricalStatus:"", disposalStatus:""},
+    {name:"Inkjet Printer", powerStatus:"good", electricalStatus:"bad", disposalStatus:""},
+    {name:"LEAN Packaging", powerStatus:"", electricalStatus:"", disposalStatus:""},
+]
+  getColour(status: String){
+    switch (status){
+      case "good":
+        return "green"
+      case "bad":
+        return "red"
+      case "warning":
+        return "yellow"
+      default:
+        return ""
+    }
+  }
   
   constructor() { 
   }
